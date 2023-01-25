348eefad5f4101c9a44fe053dd4b56f0bbcb2ddc
nithielle <105604576+nithielle@users.noreply.github.com>
2023-01-25 20:34:05 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+const botaoTrailer = document.querySelector(".botao-trailer");
+const botaoFecharModal = document.querySelector(".fechar-modal");
+//colocar querySelector no id do video pra testar
+const video = document.getElementById("video");
+const modal = document.querySelector(".modal");
+const linkDoVideo = video.scr;
+
+botaoTrailer.addEventListener("click", () => {
+    modal.classList.add("aberto");
+    video.setAttribute("src", linkDoVideo);
+}); 
+
+botaoFecharModal.addEventListener("click", () => {
+    modal.classList.remove("aberto");
+    video.setAttribute("src", "");
+})
